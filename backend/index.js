c86c4c074fe48d2de4c9f2c526b76950e903df9f
Divak-ar <144791644+Divak-ar@users.noreply.github.com>
2024-02-11 23:34:06 +0530

    connected to db, started server

---
+++ backend/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+// files
+import connectDB from "./config/db.js";
+
+// configuration
+dotenv.config();
+// dotenv loads mongoUri and sensitive env data from .env and makes it available for connectDB to use it as process varaible in this environment
+connectDB();
+
+// creates an instance of the Express.js framework, enabling you to build web applications and APIs in Node.js. I
+const app = express();
+
+// midllewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3000;
+
+// Routes
+
+app.listen(PORT, () => console.log(`Server started at the port ${PORT}`));
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,11 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-teal-700">
+        Hello world!
+      </h1>
+    </div>
+  );
 };
 
 export default App;
