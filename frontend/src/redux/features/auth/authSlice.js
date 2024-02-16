24dde47503ef6148deebe7e4754893e32786eed0
Divak-ar <144791644+Divak-ar@users.noreply.github.com>
2024-02-16 18:58:40 +0530

    made the user register page

--- frontend/src/redux/features/auth/authSlice.js
+++ frontend/src/redux/features/auth/authSlice.js
@@ -10,7 +10,9 @@ const authSlice = createSlice({
   name: "auth",
   initialState,
 
+  // after registering the data sent in the register form using regster endpoint mutation, we want to store the data in localstorage so once the user logs out and the session cookie is destroyed(done for better security prone to online CSRf and XSS attacks) we store it locally in browser so the user can easily log in the next time they visit the site
   reducers: {
+    // The setCredentials reducer updates the Redux store with user information received after successful registration.
     setCredentials: (state, action) => {
       state.userInfo = action.payload;
       localStorage.setItem("userInfo", JSON.stringify(action.payload));
